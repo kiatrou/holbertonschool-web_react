b2506058677009e68ceb8f5dcd5b05f5325bddb2
Kassandra <kassandraiatrou@gmail.com>
2025-09-19 11:07:47 +1000

    task 3

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,16 +6,33 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+// const teacher3: Teacher = {
+//     firstName: "Sherlock",
+//     fullTimeEmployee: false,
+//     lastName: "Holmes",
+//     location: "London",
+//     contract: false,
+// };
+// console.log(teacher3);
 
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+// const director1: Directors = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   location: 'London',
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+// console.log(director1);
+interface printTeacherFunction {
+    //(parameter1: type, parameter2: type): returnType
+    (firstName: string, lastName: string): string;
+}
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+function printTeacher(firstName: string, lastName: string) {
+    return firstName[0] + "." + " " + lastName
 };
-console.log(director1);
+
+console.log(printTeacher("Sherlock", "Holmes"));
