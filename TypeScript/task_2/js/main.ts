fe59ddbca8fc4e6a3b0b154b94e7c20c91df3b53
Kassandra <kassandraiatrou@gmail.com>
2025-09-19 15:08:40 +1000

    task 7

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -53,3 +53,12 @@ function executeWork(employee: Director | Teacher): void {
         employee.workTeacherTasks();
     }
 }
+
+type Subjects = "Math" | "History";
+function teachClass(todayClass: Subjects) {
+    if (todayClass == "Math") {
+        return "Teaching Math"
+    } else {
+        return "Teaching History"
+    }
+}
